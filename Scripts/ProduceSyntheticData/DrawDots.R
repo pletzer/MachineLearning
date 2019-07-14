982f9df0f044f9d2a1bdea89ee94a02202089912
Alex Pletzer <alexander@gokliya.net>
2019-07-15 11:34:24 +1200

    now attaching a unique image Id across images

--- Scripts/ProduceSyntheticData/DrawDots.R
+++ Scripts/ProduceSyntheticData/DrawDots.R
@@ -8,30 +8,39 @@ parser <- add_option(parser, c("-r", "--range"), type="integer",
                      default=5, help="Set max number of dots")
 parser <- add_option(parser, c("-s", "--seed"), type="integer",
                      default=123, help="Set random seed")
+parser <- add_option(parser, c("-d", "--directory"),
+                     default="../../Data/Synthetic/Dots", help="Set output directory")
+parser <- add_option(parser, c("-c", "--csvFile"),
+                     default="training.csv", help="Set CSV file name containing number of dots for each image")
 options <- parse_args(parser)
 
 set.seed(options$seed)
 
 
 # Where store the images
-StorageDIR = "../../Data/Synthetic/Dots"
+StorageDIR <- options$directory
 
-# range of number of points
+# range of number of dots
 range.n = c(1, options$range)
 
-for(n in seq(range.n[1], range.n[2])){
+df <- data.frame("imageId"=rep(0, options$numberOfImages), "numberOfDots"=options$numberOfImages)
 
-  dir.create(paste(StorageDIR, "/", as.character(n), sep = ""), showWarnings = FALSE)
+for(i in 1:options$numberOfImages) {
 
-  for(i in 1:options$numberOfImages){
+    numDots <- as.integer(runif(1, min=range.n[1], max=range.n[2] + 1))
+    x <- runif(numDots, min = 0, max = 1)
+    y <- runif(numDots, min = 0, max = 1)
+    extent <- runif(numDots, min = 0.5, max = 2)
 
-    x <- runif(n, min = 0, max = 1)
-    y <- runif(n, min = 0, max = 1)
+    df[i,"imageId"] = i
+    df[i, "numberOfDots"] = numDots
 
-    jpeg(filename =  paste(StorageDIR, "/", as.character(n), "/", "img", i, ".jpg", sep = ""), height = 40, width = 40)
+    jpeg(filename=paste(StorageDIR, "/", as.character(numDots), "/", "img", i, ".jpg", sep = ""), height = 40, width = 40)
     par(mai = rep(0,4))
-    plot(x,y, cex = runif(n, min = 0.5, max = 2), xlim = c(0,1), ylim = c(0,1), axes = FALSE, xlab = "", ylab = "",
+    plot(x, y, cex=extent, xlim = c(0,1), ylim = c(0,1), axes = FALSE, xlab = "", ylab = "",
          pch = 21, bg = "black", col = "white")
     dev.off()
-  }
+
 }
+
+write.csv(df, file=paste(StorageDIR, "/", options$csvFile, sep=""))
